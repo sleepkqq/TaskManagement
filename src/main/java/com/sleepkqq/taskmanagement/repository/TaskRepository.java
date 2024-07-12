d86a2e245d131fcd2667f044cb36867599b7e1cf
sleepkqq <nikita32ivanov@gmail.com>
2024-07-12 09:30:27 +0300

    fix(task repository): add @Repository annotation

--- src/main/java/com/sleepkqq/taskmanagement/repository/TaskRepository.java
+++ src/main/java/com/sleepkqq/taskmanagement/repository/TaskRepository.java
@@ -2,6 +2,8 @@ package com.sleepkqq.taskmanagement.repository;
 
 import com.sleepkqq.taskmanagement.model.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface TaskRepository extends JpaRepository<Task, Long> {
 }
