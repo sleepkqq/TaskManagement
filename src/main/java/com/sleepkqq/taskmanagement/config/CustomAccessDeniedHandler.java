2e92c770db932b12cf9d47dcb55a5637d01f35ec
sleepkqq <nikita32ivanov@gmail.com>
2024-07-31 21:52:43 +0300

    fix: code refactoring

--- src/main/java/com/sleepkqq/taskmanagement/config/CustomAccessDeniedHandler.java
+++ src/main/java/com/sleepkqq/taskmanagement/config/CustomAccessDeniedHandler.java
@@ -1,5 +1,6 @@
 package com.sleepkqq.taskmanagement.config;
 
+import io.vertx.core.json.JsonObject;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.security.access.AccessDeniedException;
@@ -8,14 +9,17 @@ import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 
+import static com.sleepkqq.taskmanagement.constants.ExceptionResponseProperties.ERROR;
+import static com.sleepkqq.taskmanagement.constants.RestProperties.APPLICATION_JSON;
+
 @Component
 public class CustomAccessDeniedHandler implements AccessDeniedHandler {
 
     @Override
     public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException {
         response.setStatus(HttpServletResponse.SC_FORBIDDEN);
-        response.setContentType("application/json");
-        response.getWriter().write("{\"error\": \"" + accessDeniedException.getMessage() + "\"}");
+        response.setContentType(APPLICATION_JSON);
+        response.getWriter().write(new JsonObject().put(ERROR, accessDeniedException.getMessage()).toString());
     }
 
 }
